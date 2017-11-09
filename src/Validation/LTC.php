ff73c5b2cfc6e894600800cda7fafa13e20dd27b
Andrey Drozd <andrey@drozd.by>
2017-11-09 21:02:42 +0300

    fix

--- src/Validation/LTC.php
+++ src/Validation/LTC.php
@@ -1,1 +1,43 @@
+<?php
 
+namespace Merkeleon\PhpCryptocurrencyAddressValidation\Validation;
+
+use Merkeleon\PhpCryptocurrencyAddressValidation\Validation;
+
+class LTC extends Validation
+{
+    const DEPRECATED_ADDRESS_VERSIONS = ['31', '05'];
+
+    protected $deprecatedAllowed = false;
+
+    protected $base58PrefixToHexVersion = [
+        'L' => '30',
+        'M' => '31',
+        '3' => '05'
+    ];
+
+    protected function validateVersion($version)
+    {
+        if (!$this->deprecatedAllowed && in_array($this->addressVersion, self::DEPRECATED_ADDRESS_VERSIONS)) {
+            return false;
+        }
+        return hexdec($version) == hexdec($this->addressVersion);
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isDeprecatedAllowed()
+    {
+        return $this->deprecatedAllowed;
+    }
+
+    /**
+     * @param boolean $deprecatedAllowed
+     */
+    public function setDeprecatedAllowed($deprecatedAllowed)
+    {
+        $this->deprecatedAllowed = $deprecatedAllowed;
+    }
+
+}
