872e4412031f4a062c1aa193a938811f8c43ce64
Andrey Drozd <andrey@drozd.by>
2017-11-09 20:26:37 +0300

    update DOGE

--- src/Validation/DOGE.php
+++ src/Validation/DOGE.php
@@ -7,23 +7,7 @@ use Merkeleon\PhpCryptocurrencyAddressValidation\Validation;
 
 class DOGE extends Validation
 {
-
-    public function validate($address)
-    {
-        $address = hex2bin(self::base58ToHex($address));
-        if (strlen($address) !== 25)
-        {
-            return false;
-        }
-        $checksum   = substr($address, 21, 4);
-        $rawAddress = substr($address, 0, 21);
-        if (substr(hex2bin(hash('sha256', hex2bin(hash('sha256', $rawAddress)))), 0, 4) === $checksum)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
+    protected $base58PrefixToHexVersion = [
+        'D' => '1E',
+    ];
 }
