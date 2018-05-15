57cc6971354dca0c3dc3110f1feb2940e974a00b
Andrey Drozd <andrey@drozd.by>
2018-05-15 16:43:09 +0300

    BCH has same format as BTC

---
+++ src/Validation/BCH.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Merkeleon\PhpCryptocurrencyAddressValidation\Validation;
+
+use Merkeleon\PhpCryptocurrencyAddressValidation\Validation;
+
+class BCH extends Validation
+{
+    // more info at https://en.bitcoin.it/wiki/List_of_address_prefixes
+    protected $base58PrefixToHexVersion = [
+        '1' => '00',
+        '3' => '05'
+    ];
+}
