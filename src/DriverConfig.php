d14f22cac4b516ffd739e2d58962f0809a763079
Ilya Shabanov <ishabanov@liquid-cats.com>
2023-04-21 17:05:09 +0400

    driver config

---
+++ src/DriverConfig.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Merkeleon\PhpCryptocurrencyAddressValidation;
+
+use Merkeleon\PhpCryptocurrencyAddressValidation\Drivers\AbstractDriver;
+use function class_exists;
+
+/**
+ * @template T
+ */
+readonly class DriverConfig
+{
+    /**
+     * @param class-string<T> $driver
+     * @param array $mainnet
+     * @param array $testnet
+     */
+    public function __construct(
+        private string $driver,
+        private array $mainnet = [],
+        private array $testnet = []
+    )
+    {
+    }
+
+    public function makeDriver(bool $isMainNet): ?AbstractDriver
+    {
+        if (!class_exists($this->driver)) {
+            return null;
+        }
+
+        return new $this->driver($this->getDriverOptions($isMainNet));
+
+    }
+
+    private function getDriverOptions(bool $isMainNet): array
+    {
+        if ($isMainNet) {
+            return $this->mainnet;
+        }
+
+        return $this->testnet
+            ?: $this->mainnet
+                ?: [];
+    }
+}
