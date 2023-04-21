03e3f23830e8aea79211cb7e7c6a18c74654bf39
Ilya Shabanov <ishabanov@liquid-cats.com>
2023-04-21 17:04:10 +0400

    config

---
+++ config/address_validation.php
@@ -0,0 +1,108 @@
+<?php
+
+declare(strict_types=1);
+
+use Merkeleon\PhpCryptocurrencyAddressValidation\DriverConfig;
+use Merkeleon\PhpCryptocurrencyAddressValidation\Drivers;
+use Merkeleon\PhpCryptocurrencyAddressValidation\Enums\CurrencyEnum;
+use Merkeleon\PhpCryptocurrencyAddressValidation\Utils\KeccakDecoder;
+
+return [
+    CurrencyEnum::BEACON->value => [
+        new DriverConfig(
+            Drivers\Bech32Driver::class,
+            ['bnb' => null],
+            ['tbnb' => null]
+        ),
+    ],
+    CurrencyEnum::BINANCE->value => [
+        new DriverConfig(KeccakDecoder::class),
+    ],
+    CurrencyEnum::BITCOIN_CASH->value => [
+        new DriverConfig(
+            Drivers\Base32Driver::class,
+            ['bitcoincash:' => null],
+            ['bchtest:' => null, 'bchreg:' => null,]
+        ),
+        new DriverConfig(
+            Drivers\DefaultBase58Driver::class,
+            ['1' => '00', '3' => '05'],
+            ['2' => 'C4', 'm' => '6F']
+        ),
+    ],
+    CurrencyEnum::BITCOIN->value => [
+        new DriverConfig(
+            Drivers\DefaultBase58Driver::class,
+            ['1' => '00', '3' => '05'],
+            ['2' => 'C4', 'm' => '6F']
+        ),
+        new DriverConfig(
+            Drivers\DefaultBase58Driver::class,
+            ['1' => '00', '3' => '05'],
+            ['2' => 'C4', 'm' => '6F']
+        ),
+    ],
+    CurrencyEnum::CARDANO->value => [
+        new DriverConfig(
+            Drivers\CardanoDriver::class,
+            ['addr1' => null],
+            ['addr_test1' => null],
+        ),
+    ],
+    CurrencyEnum::DASHCOIN->value => [
+        new DriverConfig(
+            Drivers\DefaultBase58Driver::class,
+            ['X' => '4C', '7' => '10'],
+            ['y' => '8c', '8' => '13']
+        ),
+    ],
+    CurrencyEnum::DOGECOIN->value => [
+        new DriverConfig(
+            Drivers\DefaultBase58Driver::class,
+            ['D' => '1E', '9' => '16', 'A' => '16'],
+            ['n' => '6f', 'm' => '6f', '2' => 'C4',],
+        ),
+    ],
+    CurrencyEnum::EOS->value => [
+        new DriverConfig(Drivers\EosDriver::class),
+    ],
+    CurrencyEnum::ETHEREUM_CLASSIC->value => [
+        new DriverConfig(KeccakDecoder::class),
+    ],
+    CurrencyEnum::ETHEREUM->value => [
+        new DriverConfig(KeccakDecoder::class),
+    ],
+    CurrencyEnum::LITECOIN->value => [
+        new DriverConfig(
+            Drivers\DefaultBase58Driver::class,
+            ['L' => '30', 'M' => '32', '3' => '05'],
+            ['m' => '6f', '2' => 'c4', 'Q' => '3a']
+        ),
+    ],
+    CurrencyEnum::RIPPLE->value => [
+        new DriverConfig(
+            Drivers\XrpBase58Driver::class,
+            ['r' => '00']
+        ),
+        new DriverConfig(
+            Drivers\XrpXAddressDriver::class,
+            ['X' => null],
+            ['T' => null],
+        ),
+    ],
+    CurrencyEnum::STELLAR->value => [
+        // new DriverConfig(),
+    ],
+    CurrencyEnum::TRON->value => [
+        new DriverConfig(
+            Drivers\DefaultBase58Driver::class,
+            ['T' => '41'],
+        ),
+    ],
+    CurrencyEnum::ZCASH->value => [
+        new DriverConfig(
+            Drivers\DefaultBase58Driver::class,
+            ['t' => '1C'],
+        ),
+    ],
+];
