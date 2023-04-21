53fb5b92c99dd79cd4c4abd02ecdb98c1a41b1d7
Ilya Shabanov <ishabanov@liquid-cats.com>
2023-04-21 17:05:29 +0400

    validator

---
+++ src/Validator.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Merkeleon\PhpCryptocurrencyAddressValidation;
+
+use Generator;
+use Merkeleon\PhpCryptocurrencyAddressValidation\Contracts\Driver;
+use Merkeleon\PhpCryptocurrencyAddressValidation\Enums\CurrencyEnum;
+use Merkeleon\PhpCryptocurrencyAddressValidation\Exception\AddressValidationException;
+use function app;
+use function class_exists;
+use function config;
+
+readonly class Validator implements Contracts\Validator
+{
+    public function __construct(
+        private string $chain,
+        private array  $options,
+        private bool   $isMainnet = true
+    ) {
+    }
+
+    public static function make(CurrencyEnum $currency): Validator
+    {
+        return new Validator($currency->value, config("address_validation.{$currency->value}"), app()->isProduction());
+    }
+
+    public function isValid(?string $address): bool
+    {
+        if (!$address) {
+            return false;
+        }
+
+        $drivers = $this->getDrivers();
+        // if there is no drivers we force address to be valid
+        if (null === $drivers || !$drivers->valid()) {
+            return true;
+        }
+
+        /** @var Driver $driver */
+        foreach ($drivers as $driver) {
+            if ($driver->match($address)) {
+                return $driver->check($address);
+            }
+        }
+
+        return false;
+    }
+
+    public function validate(?string $address): void
+    {
+        if (!$this->isValid($address)) {
+            throw new AddressValidationException($this->chain, $address);
+        }
+    }
+
+    /**
+     * @return Generator<int, Driver>|null
+     */
+    protected function getDrivers(): ?Generator
+    {
+        /** @var DriverConfig $driverConfig */
+        foreach ($this->options as $driverConfig) {
+            if ($driver = $driverConfig->makeDriver($this->isMainnet)) {
+                yield $driver;
+            }
+        }
+
+        return null;
+    }
+
+}
