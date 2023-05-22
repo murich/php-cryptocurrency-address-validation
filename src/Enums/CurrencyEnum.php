5e5af51b7213a6ae14502c2aa83029e958c26896
Ilya Shabanov <ishabanov@liquid-cats.com>
2023-05-22 15:28:25 +0400

    remove unused

--- src/Enums/CurrencyEnum.php
+++ src/Enums/CurrencyEnum.php
@@ -7,14 +7,12 @@ namespace Merkeleon\PhpCryptocurrencyAddressValidation\Enums;
 enum CurrencyEnum: string
 {
     case BEACON = 'beacon';
-    case BINANCE = 'binance';
     case BITCOIN_CASH = 'bitcoin_cash';
     case BITCOIN = 'bitcoin';
     case CARDANO = 'cardano';
     case DASHCOIN = 'dashcoin';
     case DOGECOIN = 'dogecoin';
     case EOS = 'eos';
-    case ETHEREUM_CLASSIC = 'ethereum_classic';
     case ETHEREUM = 'ethereum';
     case LITECOIN = 'litecoin';
     case RIPPLE = 'ripple';
