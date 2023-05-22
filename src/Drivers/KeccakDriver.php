c4e42558ec263b4e66fc79f2ab79de0483b1e473
Ilya Shabanov <ishabanov@liquid-cats.com>
2023-05-22 15:28:06 +0400

    make not strict driver

--- src/Drivers/KeccakDriver.php
+++ src/Drivers/KeccakDriver.php
@@ -6,36 +6,33 @@ namespace Merkeleon\PhpCryptocurrencyAddressValidation\Drivers;
 
 use Merkeleon\PhpCryptocurrencyAddressValidation\Utils\KeccakDecoder;
 use function intval;
-use function preg_match;
-use function str_split;
-use function strtolower;
 use function strtoupper;
-use function substr;
 
-class KeccakDriver extends AbstractDriver
+class KeccakDriver extends KeccakStrictDriver
 {
-    public function match(string $address): bool
-    {
-        return preg_match('/^0x[a-fA-F0-9]{40}$/', $address) === 1;
-    }
-
     public function check(string $address): bool
     {
-        $address = substr($address, 2);
-        $addressHash = KeccakDecoder::hash(strtolower($address), 256);
-        $addressArray = str_split($address);
-        $addressHashArray = str_split($addressHash);
+        $address = $this->toChecksum($address);
 
+        return parent::check($address);
+    }
+
+    protected function toChecksum(string $address): string
+    {
+        $address = str_replace('0x', '', $address);
+        $address = mb_strtolower($address);
+
+        $hash = KeccakDecoder::hash($address, 256);
+
+        $checksumAddress = '';
         for ($i = 0; $i < 40; $i++) {
-            // the nth letter should be uppercase if the nth digit of casemap is 1
-            if (
-                (intval($addressHashArray[$i], 16) > 7 && strtoupper($addressArray[$i]) !== $addressArray[$i]) ||
-                (intval($addressHashArray[$i], 16) <= 7 && strtolower($addressArray[$i]) !== $addressArray[$i])
-            ) {
-                return false;
+            if (intval($hash[$i], 16) >= 8) {
+                $checksumAddress .= strtoupper($address[$i]);
+            } else {
+                $checksumAddress .= $address[$i];
             }
         }
 
-        return true;
+        return '0x' . $checksumAddress;
     }
 }
