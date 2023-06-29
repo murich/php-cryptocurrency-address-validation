5b2054a9a0836bd7ca4eff17d69da2867039d6b8
Ilya Shabanov <ishabanov@liquid-cats.com>
2023-06-29 20:30:16 +0400

    aad testnet

--- config/address_validation.php
+++ config/address_validation.php
@@ -46,7 +46,8 @@ return [
         ),
         new DriverConfig(
             Drivers\CborDriver::class,
-            ['A' => 33, 'D' => 66]
+            ['A' => 33, 'D' => 66],
+            ["2" => 40, "3" => 73],
         )
     ],
     CurrencyEnum::DASHCOIN->value => [
